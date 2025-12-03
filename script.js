2fdc2f53d3744d617835a3edf1aa8c847d7c7376
SravanRudraraju <sravanrudraraju@gmail.com>
2025-12-03 10:09:14 +0530

    dblclick function added

--- script.js
+++ script.js
@@ -14,7 +14,35 @@ function savetodos(){
 
 //create a DOM node for a todo object and append it to the list
 function createTodoNode(todo,index){
+    const li = document.createElement('li')
 
+    //checkbox to toggle completion
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.checked = !!todo.completed
+    checkbox.addEventListener("change",()=>{
+        todo.completed = checkbox.checked
+
+        //visual feedback : striked
+        savetodos();
+    })
+    //text of the todo 
+    const textSpan = document.createElement("span")
+    textSpan.textContent = todo.text;
+    textSpan.style.margin = '0 px';
+    if(todo.completed){
+        textSpan.style.textDecoration = 'line-through'
+
+        //Add doubleclick event listener to edit todo
+        textSpan.addEventListener("dblclick",()=>{
+            const newText = prompt("Edit todo",todo.text);
+            if(newText != null){
+                todo.text =newText.trim()
+                textSpan.textContent = todo.text
+                savetodos()
+            }
+        })
+    }
 }
 
 //render the whole todo list from todos array
