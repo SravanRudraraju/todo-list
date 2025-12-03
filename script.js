f461cde40c4c4d49c3ca2a3bd041a3b5fe66a87b
SravanRudraraju <sravanrudraraju@gmail.com>
2025-12-03 09:50:15 +0530

    render function created

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ const todos = saved ? JSON.parse(saved): [];
 
 function savetodos(){
     //save current todos array to localstorage
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+//create a DOM node for a todo object and append it to the list
+function createTodoNode(todo,index){
+
+}
+
+//render the whole todo list from todos array
+function render(){
+    list.innerHTML = '';
+    todos.forEach((todos,index) => {
+        const node = createTodoNode(todos,index)
+        list.appendChild(node)
+
+    });
 }
